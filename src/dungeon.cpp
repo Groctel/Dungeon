3e690a4c97a3758ada2f974a685cbb40cb4af001
Groctel <groctel@protonmail.com>
2020-04-01 19:35:27 +0200

    Player now doesn't interfere with interface
    Also unified redundant sentences

--- src/dungeon.cpp
+++ src/dungeon.cpp
@@ -19,41 +19,33 @@ int main () {
 	refresh();
 
 	while ((ch = getch()) != KEY_F(1)) {
-		mvprintw(0, 0, "Y: %d/%d\nX: %d/%d\nPress [F1] to quit",
-		                y, LINES, x, COLS);
 		mvaddch(y, x, ' ');
 
 		switch (ch) {
 			case KEY_UP:
 				if (--y < 0)
 					y = 0;
-
-				mvaddch(y, x, player | A_BOLD);
 			break;
 
 			case KEY_DOWN:
 				if (++y > LINES-1)
 					y = LINES-1;
-
-				mvaddch(y, x, player | A_BOLD);
 			break;
 
 			case KEY_LEFT:
 				if (--x < 0)
 					x = 0;
-
-				mvaddch(y, x, player | A_BOLD);
 			break;
 
 			case KEY_RIGHT:
 				if (++x > COLS-1)
 					x = COLS-1;
-
-				mvaddch(y, x, player | A_BOLD);
 			break;
-			default:
-				mvaddch(y, x, player | A_BOLD);
 		}
+
+		mvprintw(0, 0, "Y: %d/%d\nX: %d/%d\nPress [F1] to quit",
+		                y, LINES, x, COLS);
+		mvaddch(y, x, player | A_BOLD);
 
 		refresh();
 	};
